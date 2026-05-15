6d94e10104b814019cd1eef70b0af2d5e5cb5013
xaexoon <sunziro03@gmail.com>
2026-05-15 17:56:36 +0900

    temp

--- src/api/config.js
+++ src/api/config.js
@@ -1,8 +1,8 @@
 // deploy
-const ERICHSEN_URL = "localhost:8000";
+// const ERICHSEN_URL = "localhost:8000";
 
 // dev
-// const ERICHSEN_URL = "192.168.1.132:8000";
+const ERICHSEN_URL = "192.168.1.132:8000";
 
 export const API_BASE = `http://${ERICHSEN_URL}`;
 export const WS_BASE = `ws://${ERICHSEN_URL}`;
